6bbab6202f5dc32d2f0aa69a84cf06176e89f10f
AmanRaSingh <developer.aman.sing>
2024-03-13 00:07:59 +0530

    some changes

--- src/Component/Sent/Sent.jsx
+++ src/Component/Sent/Sent.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Sent() {
     return (
-        <div>Sent data is here</div>
+        <div className='starred-first'>
+        <h1  class="starred-first-h1">Sent data is here</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum dignissimos recusandae quas assumenda vel eum suscipit sit laborum corrupti minus aut, sequi delectus repellendus porro, vitae accusantium doloribus cupiditate sapiente.</p>
+    </div>
     )
 }
 
--- src/Component/snoozed/Snoozed.jsx
+++ src/Component/snoozed/Snoozed.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Snoozed() {
     return (
-        <div><h1>Snoozed data is here</h1></div>
+        <div className='starred-first'>
+        <h1  class="starred-first-h1">Snoozed data is here</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum dignissimos recusandae quas assumenda vel eum suscipit sit laborum corrupti minus aut, sequi delectus repellendus porro, vitae accusantium doloribus cupiditate sapiente.</p>
+    </div>
     )
 }
 
