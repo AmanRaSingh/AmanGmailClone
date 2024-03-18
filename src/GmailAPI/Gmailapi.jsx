39c97400f5ec3ac264560f98f27c855b521075c2
AmanRaSingh <developer.aman.sing>
2024-03-18 11:32:17 +0530

    GmailAPI code

---
+++ src/GmailAPI/Gmailapi.jsx
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from 'react'
+import { json } from 'react-router-dom';
+
+function Gmailapi() {
+
+    const [accessToken, setAccessToken] = useState("");
+    const handleLogin = () => {
+        const CLIENT_ID = `360117167880-nasot64b60aseko3ergmebi4il4be5ne.apps.googleusercontent.com`;
+        const REDIRECT_URL = 'http://localhost:3000';
+        const SCOPE = 'https://www.googleapis.com/auth/gmail.readonly';
+        const AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}&scope=${SCOPE}&response_type=token`;
+
+        window.location.href = AUTH_URL;
+    }
+
+    const getAccessToken = () => {
+        const url = window.location.href;
+        const token = url.match(/access_token=([^&]+)/);
+        if (token && token.length > 1) {
+            localStorage.setItem("Token", token[1]);
+        }
+    }
+    useEffect(() => {
+        getAccessToken()
+    }, []);
+
+    const getEmailData = () => {
+        let token = localStorage.getItem("Token")
+        console.log("hello", token);
+        let url = "https.//gmail.googleapis.com/gmail/v1/users/me/message"
+
+        const options = {
+            method: "GET",
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': `application/json`
+            }
+        }
+        fetch(url, options)
+            .then(response => response.json())
+            .then(json => console.log(fetchMail(json.message[0].id)))
+            .catch(error => console.log('Error in fetching mails', error))
+    }
+    const fetchMail = (id) => {
+        let token = localStorage.getItem("Token")
+        const options = {
+            method: "GET",
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': `application/json`
+            }
+        }
+        let url = "https://gmail.googleapis.com/gmail/v1/users/me/messages/${id}";
+        fetch(url, options)
+            .then(response => response.json())
+            .then(json => console.log(json))
+            .catch(error => console.log("Error in fetching mails"))
+    }
+    return (
+        <>
+            <div>
+                {accessToken ? (
+                    <div>
+                        <h1>Access token obtained</h1>
+                        <p>Access token :{accessToken}</p>
+                    </div>) : (
+                    <>
+                        <button onClick={handleLogin}> Login with Google</button>
+                        <button onClick={() => getEmailData()}> Get Email</button>
+                        <button onClick={() => fetchMail()}>Fetch Emails</button>
+                    </>
+                )
+                };
+            </div>
+        </>
+    )
+}
+
+export default Gmailapi
